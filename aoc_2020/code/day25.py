4f5416b00ccc2b8ff6a421c2746bec469e9c5966
hoehlenwolf <35019735+hoehlenwolf@users.noreply.github.com>
2020-12-30 09:03:45 +0100

    2020 Day 25 Part A

--- aoc_2020/code/day25.py
+++ aoc_2020/code/day25.py
@@ -5,12 +5,32 @@ _INPUT_PATH = Path(dirname(realpath(__file__))).parent / Path("inputs") / Path("
 # load puzzle input
 with open(_INPUT_PATH, 'r') as f:
     lines = [line.replace("\n", "") for line in f.readlines()]
-
+pub_key_1 = int(lines[0])
+pub_key_2 = int(lines[1])
 
 ####################################################################################################
+def find_loopsize(subject_number, target):
+    val = 1
+    loop_size = 0
+    while True:
+        val *= subject_number
+        val = val % 20201227
+        loop_size += 1
+        if val == target:
+            return loop_size
+def transform_subject(subject_number, loop_size):
+    val=1
+    for i in range(loop_size):
+        val *= subject_number
+        val = val % 20201227
+    return val
 def part_a():
     """Part A"""
-    return None
+    loop_size = find_loopsize(7, pub_key_1)
+    encryption_key = transform_subject(pub_key_2, loop_size)
+    return encryption_key
+
+
 
 
 def part_b():
